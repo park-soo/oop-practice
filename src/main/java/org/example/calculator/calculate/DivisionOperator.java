312e784b4407f962b1511c92677cdec834f04908
parkmansoo <parkman.xxx@gmail.com>
2023-08-03 00:22:00 +0900

    사칙연산 계산기 구현  * 요구사항  * 간단한 사칙연산을 할 수 있다.  * 양수로만 계산할 수 있다.  * 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.  * MVC패턴(Model-View-Controller) 기반으로 구현한다.
    -- 객체지향적으로 구현

---
+++ src/main/java/org/example/calculator/calculate/DivisionOperator.java
@@ -0,0 +1,19 @@
+package org.example.calculator.calculate;
+
+public class DivisionOperator implements NewArithmeticOperator {
+    @Override
+    public boolean supports(String operator) {
+        return "/".equals(operator);
+    }
+
+    @Override
+    public int calculate(PositiveNumber operand1, PositiveNumber operand2) {
+
+        // PositiveNumber에서 이미 0에 대한 예외처리를 진행함..(무조건 양수임을 보장)
+//        if (operand2.toInt() == 0) {
+//            throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+//        }
+
+        return operand1.toInt() / operand2.toInt();
+    }
+}
